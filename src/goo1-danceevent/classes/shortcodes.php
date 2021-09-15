95b983da233af98c60e91b056b98b34d72617601
Andreas Kasper <andreas.kasper@goo1.de>
2021-09-15 23:33:28 +0200

    Umbau auf js statt direkt IFRAME inkl noscript

--- src/goo1-danceevent/classes/shortcodes.php
+++ src/goo1-danceevent/classes/shortcodes.php
@@ -7,12 +7,14 @@ class shortcodes {
   public static function render_livestream($atts, $content) {
     //$round_id = $_GET["round"] ?? null;
 
-    $url = "https://5678.video/wordpress/embed/livestream?domain=".urlencode($_SERVER["HTTP_HOST"]);
+    $url = "https://5678.video/wordpress/embed/livestream.js?domain=".urlencode($atts["domain"] ?? $_SERVER["HTTP_HOST"] ?? null);
+    $url2 = "https://5678.video/wordpress/embed/livestream?domain=".urlencode($atts["domain"] ?? $_SERVER["HTTP_HOST"] ?? null);
 
 	  $html .= '';
-	  $html .= '<div style="position: relative; background: #000; width: 100%; height: 0; padding-bottom: 56.25%; border: none;">';
-    $html .= '<IFRAME src="'.$url.'" FRAMEBORDER="0" style="position: absolute; display: block; left: 0; top: 0; width: 100%; height: 100%; background: #000; color: #aaa;"></IFRAME>';
-    $html .= '</div>';
+	  $html .= '<script src="'.$url.'"></script>';
+    $html .= '<noscript><div style="position: relative; background: #000; width: 100%; height: 0; padding-bottom: 56.25%; border: none;">';
+    $html .= '<IFRAME src="'.$url2.'" FRAMEBORDER="0" style="position: absolute; display: block; left: 0; top: 0; width: 100%; height: 100%; background: #000; color: #aaa;"></IFRAME>';
+    $html .= '</div></noscript>';
     return $html;
   }
 
